b2c50f729527371e86920b3e2c3fc8c233881c36
Ruel Savandal <davidbogs888@gmail.com>
2021-03-27 08:47:39 +0800

    share code on github

---
+++ src/data.js
@@ -0,0 +1,106 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'S.D Brownie Bites',
+            category:'Foods',
+            images:'/images/p1.jpg',
+            price:50,
+            brand:'Super Delight',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'2',
+            name:'BIG ROLLS Choco & Cheese',
+            category:'Foods',
+            images:'/images/p2.jpg',
+            price:150,
+            brand:'BIG ROLLS',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'3',
+            name:'SNACKERS Barquillos',
+            category:'Foods',
+            images:'/images/p3.jpg',
+            price:85,
+            brand:'SNACKERS',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'4',
+            name:'S.D Chocolate Cip Cookies',
+            category:'Foods',
+            images:'/images/p4.jpg',
+            price:135,
+            brand:'Super Delight',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'5',
+            name:'Black & White',
+            category:'Foods',
+            images:'/images/p5.jpg',
+            price:95,
+            brand:'Fibisco',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'6',
+            name:'Wafer Choco & Vanilla',
+            category:'Foods',
+            images:'/images/p6.jpg',
+            price:50,
+            brand:'Wafer',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'7',
+            name:'Pringles',
+            category:'Foods',
+            images:'/images/p7.jpg',
+            price:120,
+            brand:'Pringles',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+        {
+            _id:'8',
+            name:'Fibisco Chocolate Chip Cookies',
+            category:'Foods',
+            images:'/images/p8.jpg',
+            price:100,
+            brand:'Fibisco',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+
+        },
+        {
+            _id:'9',
+            name:'LEXI Primium Chocolate',
+            category:'Foods',
+            images:'/images/p9.jpg',
+            price:75,
+            brand:'LEXI',
+            rating:5,
+            numReviews:10,
+            description:'Taste Good!'
+        },
+
+    ],
+};
+export default data;
